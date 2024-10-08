eee9dc7f942e6e99e36c239b2e96de5e507901cb
Wertz <parkerwertz123@gmail.com>
2024-10-08 08:26:30 -0400

    Added a While Loop for our OS booting up w/ ellipsis

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,1 +1,19 @@
-print("\nWelcome to InfoTechCenter V1.0")
+import sys
+import time
+
+print("\nWelcome to InfoTechCenter V1.0\n")
+
+
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    message = ("InfoTech Center System Booting" + "." * ellipsis)
+    ellipsis += 1
+    sys.stdout.write("\r" + message)
+    time.sleep(.5)
+    if ellipsis == 4:
+        ellipsis = 0
+    if x == 20:
+        print("\n\nOperating System Booted Up - Retina Scanned - Access Granted")
